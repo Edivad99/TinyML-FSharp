71b6c24ac7712a6fd0b746204351d110e60c3476
Davide Albiero <davide2910@live.it>
2023-01-30 13:41:47 +0100

    Aggiunti i test

---
+++ TinyML.Test/Evaluate.fs
@@ -0,0 +1,24 @@
+﻿namespace TinyML.Test
+
+open TinyML
+open Ast
+
+module Evaluate =
+
+    let mutable tenv = []
+    let mutable venv = []
+
+    let evaluate line = 
+        let x, (t, v) =
+            match Main.parse_from_string line "LINE" Parser.interactive with 
+            | IExpr e ->
+                "it", Main.interpret_expr tenv venv e
+
+            | IBinding (_, x, _, _ as b) ->
+                let t, v = Main.interpret_expr tenv venv (LetIn (b, Var x))
+                let tmp = Forall(Set.empty, t)
+                tenv <- (x, tmp) :: tenv
+                venv <- (x, v) :: venv
+                x, (t, v)
+        x, pretty_ty t, pretty_value v
+
---
+++ TinyML.Test/TestLet.fs
@@ -0,0 +1,54 @@
+﻿namespace TinyML.Test
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestLet () =
+
+    [<TestMethod>]
+    member _.TestLetInt () =
+        let t, l, r = "int", "x", "2"
+        let variable_name, ty, v = Evaluate.evaluate $"let {l} = {r};;"
+        Assert.AreEqual(l, variable_name)
+        Assert.AreEqual(t, ty)
+        Assert.AreEqual(v, r);
+
+    [<TestMethod>]
+    member _.TestLetFloat () =
+        let t, l, r = "float", "x", "0.5"
+        let variable_name, ty, v = Evaluate.evaluate $"let {l} = {r};;"
+        Assert.AreEqual(l, variable_name)
+        Assert.AreEqual(t, ty)
+        Assert.AreEqual(v, r);
+
+    [<TestMethod>]
+    member _.TestLetString () =
+        let t, l, r = "string", "x", "\"ciao\""
+        let variable_name, ty, v = Evaluate.evaluate $"let {l} = {r};;"
+        Assert.AreEqual(l, variable_name)
+        Assert.AreEqual(t, ty)
+        Assert.AreEqual(v, r);
+
+    [<TestMethod>]
+    member _.TestLetChar () =
+        let t, l, r = "char", "x", "c"
+        let variable_name, ty, v = Evaluate.evaluate $"let {l} = '{r}';;"
+        Assert.AreEqual(l, variable_name)
+        Assert.AreEqual(t, ty)
+        Assert.AreEqual(v, r);
+
+    [<TestMethod>]
+    member _.TestLetBool () =
+        let t, l, r = "bool", "x", "true"
+        let variable_name, ty, v = Evaluate.evaluate $"let {l} = {r};;"
+        Assert.AreEqual(l, variable_name)
+        Assert.AreEqual(t, ty)
+        Assert.AreEqual(v, r);
+
+    [<TestMethod>]
+    member _.TestLetUnit () =
+        let t, l, r = "unit", "x", "()"
+        let variable_name, ty, v = Evaluate.evaluate $"let {l} = {r};;"
+        Assert.AreEqual(l, variable_name)
+        Assert.AreEqual(t, ty)
+        Assert.AreEqual(v, r);
