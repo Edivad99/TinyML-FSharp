9d288df763883adea7c79ab79ce0be3da4465af3
Davide Albiero <davide2910@live.it>
2023-02-06 19:14:58 +0100

    Aggiunti test

--- TinyML.Test/TestLambda.fs
+++ TinyML.Test/TestLambda.fs
@@ -46,3 +46,51 @@ type TestLambda () =
         Assert.AreEqual("f", variable_name)
         Assert.AreEqual("int -> int -> (int, int)", ty)
         Assert.AreEqual("<|[];x;fun y -> (if true then x else y, x + 1)|>", v)
+
+    [<TestMethod>]
+    member _.TestLambdaHighOrderFullPolymorphic () =
+        let variable_name, ty, v = Evaluate.evaluate $"""
+            let f x y = x y
+            in (f (fun x -> x), f (fun x -> x) 1, f (fun x -> x) "ciao");;
+        """
+        Assert.AreEqual("it", variable_name)
+        Assert.AreEqual("('6 -> '6, int, string)", ty)
+        Assert.AreEqual("<|[x=<|[f=<|[];x;fun y -> x y|>];x;x|>];y;x y|>, 1, \"ciao\"", v)
+
+    [<TestMethod>]
+    member _.TestLambdaHighOrderWhichReceivesPolymorphicFunction () =
+        let variable_name, ty, v = Evaluate.evaluate $"""
+            let f = fun x -> 
+                fun y ->
+                    if x > 0
+                    then y x
+                    else y (x + 1)
+            in f 5 (fun x -> x);;
+        """
+        Assert.AreEqual("it", variable_name)
+        Assert.AreEqual("int", ty)
+        Assert.AreEqual("5", v)
+
+    [<TestMethod>]
+    member _.TestLambdaReturningClusureArgsShadowed () =
+        let variable_name, ty, v = Evaluate.evaluate $"""
+            let g = fun x -> ( x + 1, let x = "ciao" in x , fun x -> x )
+            in g 5;;
+        """
+        Assert.AreEqual("it", variable_name)
+        Assert.AreEqual("(int, string, '3 -> '3)", ty)
+        Assert.AreEqual("6, \"ciao\", <|[x=5];x;x|>", v)
+
+    [<TestMethod>]
+    member _.PolymorphicHighOrderFunctionMoreTimesTuple () =
+        let variable_name, ty, v = Evaluate.evaluate $"""
+            let f = fun x ->
+                fun y ->
+                    if x > 0
+                    then y x
+                    else y (x + 1)
+            in (f, f (-3) (fun x -> x), f (-5) (fun x -> x + 1), f);;
+        """
+        Assert.AreEqual("it", variable_name)
+        Assert.AreEqual("(int -> int -> '5 -> '5, int, int, int -> int -> '14 -> '14)", ty)
+        Assert.AreEqual("<|[];x;fun y -> if x > 0 then y x else y x + 1|>, -2, -3, <|[];x;fun y -> if x > 0 then y x else y x + 1|>", v)
---
+++ TinyML.Test/TestLetRec.fs
@@ -0,0 +1,74 @@
+﻿namespace TinyML.Test
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open TinyML
+
+[<TestClass>]
+type TestLetRec () =
+
+    [<TestInitialize>]
+    member _.TestInitialize () =
+        Evaluate.tenv <- []
+        Evaluate.venv <- []
+        Typing.reset_var_counter()
+
+    [<TestMethod>]
+    member _.TestLetRec () =
+        let variable_name, ty, v = Evaluate.evaluate $"let rec f x = if x < 5 then 5 else f x + 1;;"
+        Assert.AreEqual("f", variable_name)
+        Assert.AreEqual("int -> int", ty)
+        Assert.AreEqual("<|[];f;x;if x < 5 then 5 else f x + 1|>", v)
+
+    [<TestMethod>]
+    member _.TestLetRecFibonacci () =
+        let variable_name, ty, v = Evaluate.evaluate $"""
+            let rec fib x =
+                if x < 3 then 1
+                else fib (x - 1) + fib (x - 2);;
+        """
+        Assert.AreEqual("fib", variable_name)
+        Assert.AreEqual("int -> int", ty)
+        Assert.AreEqual("<|[];fib;x;if x < 3 then 1 else fib x - 1 + fib x - 2|>", v)
+
+    [<TestMethod>]
+    member _.RecursivePolymorphicFunctionAppliedMultipleOnATuple2 () =
+        let variable_name, ty, v = Evaluate.evaluate $"""
+            let z = 2 in
+            let rec f x y = x (y + 1)
+                in (
+                    f (fun x -> x) 5,
+                    f,
+                    f (fun (x :int) -> if (x - 1) > 0 then x/z else x*z) -5,
+                    f (fun (x :int) -> if x > 1 then ()) -5,
+                    f (fun x -> "ciao"),
+                    f
+                );;
+        """
+        Assert.AreEqual("it", variable_name)
+        Assert.AreEqual("(int, int -> '9 -> int -> '9, int, unit, int -> string, int -> '21 -> int -> '21)", ty)
+        Assert.AreEqual("<|[z=2];f;x;fun y -> x y + 1|>", v)
+
+    [<TestMethod>]
+    member _.RecursivePolymorphicFunctionAppliedMultipleTimesLetBindings () =
+        let variable_name, ty, v = Evaluate.evaluate $"""
+            let z = 2 in
+            let rec f x y = 
+                x (y + 1)
+            in let g = f (fun x -> x) 5
+            in let w = f
+            in let z = f (fun x -> x/z)
+            in let s = f (fun x -> "ciao")
+            in let a = f
+            in (g, w, z, s, a);;
+        """
+        Assert.AreEqual("it", variable_name)
+        Assert.AreEqual("(int, int -> '17 -> int -> '17, int -> int, int -> string, int -> '18 -> int -> '18)", ty)
+        Assert.AreEqual("<|[z=2];f;x;fun y -> x y + 1|>", v)
+
+    [<TestMethod>]
+    member _.RecursiveFunctionCountStep () =
+        let _ = Evaluate.evaluate $"""let rec f x y = if x > 0 then f (x - 1) (y + 1) else y;;"""
+        let variable_name, ty, v = Evaluate.evaluate $"f 4 0;;"
+        Assert.AreEqual("it", variable_name)
+        Assert.AreEqual("int", ty)
+        Assert.AreEqual("4", v)
